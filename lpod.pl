59cc68015d2a6af99eb319aa048a7f1b2f16ebb8
Angelos Charalambidis <acharal@hua.gr>
2024-06-21 07:30:26 +0300

    Updates

--- lpod.pl
+++ lpod.pl
@@ -1,3 +1,6 @@
+#!/usr/bin/env swipl
+
+
 :- use_module(library(readutil)).
 :- op(100, xfx, {}).
 :- op(100, fx, #).
@@ -9,33 +12,23 @@
 :- op(1100, xfx, show).
 :- op(1100, xfx, external).
 
-% read LPODS
 
-load_file(F,T) :- see(F), read_clauses([],T), seen.
-
-read_clauses(L1,L) :- read(C),(
-        C = end_of_file, !, L = L1
-        ; append(L1,[C],L2),
-          read_clauses(L2,L)
-        ).
-
-compile_lpod(File, CompiledRules) :-
-	load_file(File, Rules),
-	translate_lpod(Rules, CompiledRules).
-
+headsym('h').
+bodysym('b').
+fstarsym('isF').
+fstar_or_true_sym('isForT').
 
 translate_lpod(Rules, CompiledRules) :-
-	% classify_rules(Rules, Prefs, _),
 	normalize_rules(Rules, NormalRules),
-	classify_rules(NormalRules, Prefs, _),
-	translate_prefs(Prefs, CompiledPrefs),
-	% gen_fstars(NormalRules, CompiledFstars),
-	CompiledFstars = [],
-	append(CompiledPrefs, CompiledFstars, CompiledRules).
+	classify_rules(NormalRules, Prefs, Regs),
+	translate_prefs(Prefs, CompilePrefs),
+	append(CompilePrefs, Regs, CompileRules0),
+	gen_fstar_rules(NormalRules, CompileFstars),
+	append(CompileRules0, CompileFstars, CompiledRules).
 
 % Classifies rules to regulars and with preferences (ordered disjunction)
 classify_rules([],[],[]).
-classify_rules([R|Rs], [R|Ps], Os) :- is_pref(R), !, classify_rules(Rs, Ps, Os).
+classify_rules([R|Rs], [R|Ps], Os) :- odisj_rule(R), !, classify_rules(Rs, Ps, Os).
 classify_rules([R|Rs], Ps, [R|Os]) :- classify_rules(Rs, Ps, Os).
 
 normalize_rules([],[]).
@@ -45,40 +38,44 @@ normalize_rules([R|Rs], NormalizedRules) :-
 	append(NR, NRs, NormalizedRules).
 
 normalize_rule((:- Body),    [(:- Body)]).
-normalize_rule((Head:-Body), [(Head :- Body)]) :- atom(Head), !.
-normalize_rule((Head:-Body), [(Head :- Body1) | Aux ]) :- 
-	Head = (_ ; _), 
+normalize_rule((Head:-Body), [(Head :- Body)]) :- lit(Head), !.
+normalize_rule((Head:-Body), [(Head :- Body1) | Aux ]) :- disj(Head), !, 
 	gensym('r', R), 
 	normalize_body(R, Body, Body1, Aux).
-normalize_rule((Head:-Body), [(Head1 :- Body1) | Aux ]) :- 
-	Head = (_ x _),  
+normalize_rule((Head:-Body), [(Head1 :- Body1) | Aux ]) :- odisj(Head), !,
 	gensym('r', R), 
 	normalize_body(R, Body, Body1, Aux1),
 	normalize_head(R, Head, Head1, Aux2),
 	append(Aux1, Aux2, Aux).
 
-normalize_body(_, Body, Body,  []) :- atom(Body), !.
-normalize_body(R, Body, Body1, [Body1 :- Body]) :- Body1 =.. ['b', R].
+normalize_body(_, Body, Body,  []) :- lit(Body), !.
+normalize_body(R, Body, Body1, [Body1 :- Body]) :- conj(Body), !, bodysym(S), Body1 =.. [S, R].
 
-normalize_head(_, Head, Head, [])  :- atom(Head), !.
-normalize_head(_, Head, Head, [])  :- Head = (_ ; _), !.
-normalize_head(R, Head, Head1, Aux) :- Head = (_ x _), 
+normalize_head(_, Head, Head, [])  :- lit(Head), !.
+normalize_head(_, Head, Head, [])  :- disj(Head), !.
+normalize_head(R, Head, Head1, Aux) :- odisj(Head), !, 
 	bin_to_list('x', Head, HeadList),
 	normalize_head_aux(R, HeadList, NormalHeadList, Aux),
 	binop('x', NormalHeadList, Head1).
 
 normalize_head_aux(_, [],    [], []).
-normalize_head_aux(R, [H|L], [H|NL], Aux) :- 
-	atom(H), !, 
+normalize_head_aux(R, [H|L], [H|NL], Aux) :- lit(H), !, 
 	normalize_head_aux(R, L, NL, Aux).
 normalize_head_aux(R, [H|L], [NH|NL], [ (H :- NH) |Aux]) :-
 	gensym('o', O),
-	NH =.. [ 'h', R, O ],
+	headsym(S),
+	NH =.. [ S, R, O ],
 	normalize_head_aux(R, L, NL, Aux).
 
-is_disj((_ ; _) :- _).
-is_pref((_ x _) :- _).
-is_not((not _)).
+conj(_ , _).
+disj(_ ; _).
+odisj(_ x _).
+neg(not _).
+lit(L) :- atom(L).
+lit(L) :- compound(L), functor(L, F, A), L \= ';', L \= ','.
+
+disj_rule(Head :- _) :- disj(Head).
+odisj_rule(Head :- _) :- odisj(Head).
 
 translate_prefs([],     []). 
 translate_prefs([P|Ps], Rs) :- 
@@ -88,40 +85,38 @@ translate_prefs([P|Ps], Rs) :-
 
 translate_pref_rule((Head:-Body), Rules) :-
 	bin_to_list('x', Head, Options),
-	translate_body(Body, Body1, BodyRules),
-	translate_options(Body1, Options, OptionRules),
-	append(OptionRules, BodyRules, Rules).
-
-% translate_body(Body, Body, []) :- atom(Body), !.
-% translate_body(Body, B, [ (B :- Body) ]) :- gensym(r, RSymbol), B =.. [body, RSymbol].
-
-% translate_head(O, O, [])  :- atom(O), !. % or a single predicate.
-% translate_head(O, S, [ (O :- S) ]) :- gensym(head, S).
+	translate_options(Body, Options, Rules).
 
 translate_options(Body, Options, Rules) :-
 	translate_options(Body, Options, [], Rules).
 
 translate_options(_,    [],  _,    []).
-translate_options(Body, [O], Prev, RR) :-
-	translate_head(O, O1, OR), 
-	append_to_rule((O1 :- Body), Prev, R), 
-	append([R], OR, RR),
-	!.
+translate_options(Body, [O], Prev, [R]) :-
+	append_to_rule((O :- Body), Prev, R), !.
 translate_options(Body, [O|Os], Prev, Rs) :-
-	translate_head(O, O1, OR), 
-	append_to_rule(({ O1 } :- Body), Prev, R),
+	append_to_rule(({ O } :- Body), Prev, R),
 	translate_options(Body, Os, [(not O)|Prev], RestR),
-	append([R], OR, RR),
-	append(RR, RestR, Rs).
+	append([R], RestR, Rs).
 
-gen_fstars((Head:-Body), FstarRules) :-
+gen_fstar_rules([], []).
+gen_fstar_rules([R|Rs], FRR) :-
+	gen_fstar_rule(R, FR),
+	gen_fstar_rules(Rs,FRs),
+	append(FR, FRs, FRR).
+
+gen_fstar_rule((Head:-Body), FstarRules) :-
+	Head = (_ x _), !,
 	bin_to_list('x', Head, Options),
-	gen_fstars_options(Body, Options, [], FstarRules0),
-	gen_fstars_body(Body, RealBody, FstarRules1),
-	gen_fstars_head(Head, RealHead, FstarRules2),
-	append(FstarRules0, FstarRules1, FstarRules3),
-	append(FstarRules3, FstarRules2, FstarRules).
+	gen_fstars_options(Body, Options, [], FstarRules).
 
+gen_fstar_rule((Head:-Body), FstarRules) :-
+	Head = (_ ; _), !,
+	gen_fstars_head(Body, Head, FstarRules).
+
+gen_fstar_rule((Head:-Body), FstarRules) :- !,
+	gen_fstars_body(Head, Body, FstarRules).
+
+gen_fstars_options(_, [], _, []).
 gen_fstars_options(Body, [O|Os], Prev, Rs) :-
 	% neg_list(Prev, NPrev), 
 	append_to_rule((isF(O)    :- (not O), Body),      Prev, R0),
@@ -134,8 +129,8 @@ gen_fstars_options(Body, [O|Os], Prev, Rs) :-
 gen_fstars_body(BodySymbol, RealBody, Rs) :-
 	bin_to_list(',', RealBody, BodyList),
 	body_to_posneg(BodyList, PosList, NegList),
-	mapop(isForT, PosList, isForTList),
-	append(isForTList, NegList, L),
+	mapop(isForT, PosList, ForTList),
+	append(ForTList, NegList, L),
 	gen_fstars_or_trues(PosList,PosListRules),
 	append_to_rule((isF(BodySymbol) :- not BodySymbol), L, R),
 	append([R], PosListRules, Rs).
@@ -146,9 +141,8 @@ gen_fstars_or_trues([L|Ls],Rs) :-
 	gen_fstars_or_trues(Ls,Rs1),
 	append(Rs0, Rs1, Rs).
 
-gen_fstars_head(RealHead, RealHead, []) :- atom(RealHead), !.
 gen_fstars_head(HeadSymbol, RealHead, Rs) :-
-	bin_to_list(';', ReadHead, HeadList),
+	bin_to_list(';', RealHead, HeadList),
 	mapop(isF, HeadList, IsFList),
 	binop(';', IsFList, IsFHead),
 	Rs = [(IsFHead :- isF(HeadSymbol))].
@@ -170,8 +164,30 @@ append_to_rule((H :- B), L, (H :- B1)):-
 	append(Bs0, L, Bs),
 	binop(',', Bs, B1).
 
-neg_list([],[]).
-neg_list([X|Xs], [(not X) | NotXs]) :- neg_list(Xs, NotXs).
 
-body_to_posneg([L|Ls], Pos, [L|Neg]) :- is_not(L), !, body_to_posneg(Ls, Pos, Neg).
+% unused
+neg_list(X,Y) :- mapop(not, X, Y). 
+
+body_to_posneg([], [], []).
+body_to_posneg([L|Ls], Pos, [L|Neg]) :- neg(L), !, body_to_posneg(Ls, Pos, Neg).
 body_to_posneg([L|Ls], [L|Pos], Neg) :- body_to_posneg(Ls, Pos, Neg).
+
+
+% read LPODS
+
+load_file(F,T) :- see(F), read_rules([],T), seen.
+
+read_rules(L1,L) :- read(C),(
+        C = end_of_file, !, L = L1
+        ; append(L1,[C],L2),
+          read_rules(L2,L)
+        ).
+
+write_rules([]) :- !.
+write_rules([R|Rs]) :- write(R), write('.'), nl, write_rules(Rs). 
+
+compile_lpod(File, CompiledRules) :-
+	load_file(File, Rules),
+	translate_lpod(Rules, CompiledRules).
+
+main(Argv).
