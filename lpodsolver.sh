94f47cc88ba52029ba72276ab90f03c28933c28e
grnkl <sdi2000153@di.uoa.gr>
2024-07-09 10:25:23 +0300

    Fix file naming

--- lpodsolver.sh
+++ lpodsolver.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [[ -z "$1" ]]; then
-    echo "Usage: ./lpodsolver.sh example.lpod"
+    echo "Usage: ./lpodsolver.sh ./example.lpod"
     exit 1
 fi
 
-name=$(basename $1)
+name="${1%.*}"
 
 swipl ./lpod.pl $1 > $name.lp
 asprin $name.lp ./pref.lp 0
